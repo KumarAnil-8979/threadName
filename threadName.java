a8b03794b2e1000d75153c8542a6b425bfaa3bbc
Anil Kumar <85837828+KumarAnil-8979@users.noreply.github.com>
2022-08-08 13:06:51 +0530

    Add files via upload

---
+++ threadName.java
@@ -0,0 +1,10 @@
+public class threadName extends Thread{
+    public   void run(){
+        System.out.println(this.getName());
+    }
+    public static void main(String[] args) {
+        threadName t=new threadName();
+        t.start();
+
+    }
+}
